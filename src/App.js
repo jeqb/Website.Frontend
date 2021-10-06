d3a3566982f6795c8900cfcea9569ac2ad9c5119
James <jameseqbonner@protonmail.com>
2021-10-05 17:29:31 -0700

    added Contact page with routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
 import Header from './components/header/Header';
 import RetroBackground from './components/background/RetroBackground';
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
 import NotFound from './components/not-found/NotFound';
 
 
@@ -16,6 +17,7 @@ const App = () => {
       <Switch>
         <Redirect exact from='/' to='/about'/>
         <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
         <Route component={NotFound} />
       </Switch>
     </div>
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <div className="Content-Container">
+      <h1>
+        Contact
+      </h1>
+
+    </div>
+  )
+};
+
+export default Contact;

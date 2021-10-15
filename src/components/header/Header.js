4c8867fda0f8d538bdbe7ae85719aa1b48b17e33
James <jameseqbonner@protonmail.com>
2021-10-14 20:11:32 -0700

    making progress on the header/navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter, useHistory  } from 'react-router-dom';
 
 import './Header.css';
 
 const Header = () => {
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
   const history = useHistory();
 
@@ -14,26 +17,53 @@ const Header = () => {
   };
 
   return (
-    <div className="Header">
-      <div
-        className="Button End"
-        onClick={() => handleClick('/contact')}
-      >
-        Contact
+    <nav className="navbar navbar-expand-lg navbar-expand-md navbar-dark ">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <div
+                className="nav-link active Button"
+                aria-current="page"
+                onClick={() => handleClick('/about')}
+              >
+                About
+              </div>
+            </li>
+            <li className="nav-item">
+              <div
+                className="nav-link active Button"
+                aria-current="page"
+                onClick={() => handleClick('/services')}
+              >
+                Services
+              </div>
+            </li>
+            <li className="nav-item">
+              <div
+                  className="nav-link active Button"
+                  aria-current="page"
+                  onClick={() => handleClick('/contact')}
+                >
+                  Contact
+                </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div
-        className="Button"
-        onClick={() => handleClick('/services')}
-      >
-        Services
-      </div>
-      <div
-        className="Button"
-        onClick={() => handleClick('/about')}
-      >
-        About
-      </div>
-    </div>
+    </nav>
   );
 };
 

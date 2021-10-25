26b57fd9e825b3e1107b01d2ceb918ed355d7d97
James <jameseqbonner@protonmail.com>
2021-10-24 21:21:52 -0700

    nailed the breakpoint.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -18,63 +18,61 @@ const Contact = () => {
   // onClick={e => { handleSubmit(e) }}
 
   return (
-    <div className="align-middle" style={{maxHeight: '100vh'}}>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-2 col-sm-12">
-            <h1 className="text-center">
-              Contact
-            </h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-lg-2 col-md-2 col-sm-12">
+          <h1 className="text-center">
+            Contact
+          </h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-2 col-md-2">
+          {/* spacer */}
+        </div>
+        <div className="col-lg-4 col-md-4 col-sm-12">
+          <label className="form-label" for="name">
+            Name
+          </label>
+          <input className="form-control" id="name" type="text" placeholder="Name" data-sb-validations="required" />
+          <div className="invalid-feedback" data-sb-feedback="name:required">
+            Name is required.
           </div>
         </div>
-        <div className="row">
-          <div className="col-lg-2">
-            {/* spacer */}
+        <div className="col-lg-4 col-md-4 col-sm-12">
+          <label className="form-label" for="emailAddress">Email Address</label>
+          <input className="form-control" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required, email" />
+          <div className="invalid-feedback" data-sb-feedback="emailAddress:required">
+            Email Address is required.
           </div>
-          <div className="col-lg-4 col-sm-12">
-            <label className="form-label" for="name">
-              Name
-            </label>
-            <input className="form-control" id="name" type="text" placeholder="Name" data-sb-validations="required" />
-            <div className="invalid-feedback" data-sb-feedback="name:required">
-              Name is required.
-            </div>
-          </div>
-          <div className="col-lg-4 col-sm-12">
-            <label className="form-label" for="emailAddress">Email Address</label>
-            <input className="form-control" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required, email" />
-            <div className="invalid-feedback" data-sb-feedback="emailAddress:required">
-              Email Address is required.
-            </div>
-            <div className="invalid-feedback" data-sb-feedback="emailAddress:email">
-              Email Address Email is not valid.
-            </div>
-          </div>
-          <div className="col-lg-2">
-            {/* spacer */}
+          <div className="invalid-feedback" data-sb-feedback="emailAddress:email">
+            Email Address Email is not valid.
           </div>
         </div>
-        <div className="row">
-          <div className="col-lg-2">
-            {/* spacer */}
+        <div className="col-lg-2 col-md-2">
+          {/* spacer */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-2 col-md-2">
+          {/* spacer */}
+        </div>
+        <div className="col-lg-8 col-md-8 col-sm-12">
+          <label className="form-label" for="message">
+            Message
+          </label>
+          <textarea className="form-control" id="message" type="text" placeholder="Message" style={{height: '10rem'}} data-sb-validations="required"></textarea>
+          <div className="invalid-feedback" data-sb-feedback="message:required">
+            Message is required.
           </div>
-          <div className="col-lg-8 col-sm-12">
-            <label className="form-label" for="message">
-              Message
-            </label>
-            <textarea className="form-control" id="message" type="text" placeholder="Message" style={{height: '10rem'}} data-sb-validations="required"></textarea>
-            <div className="invalid-feedback" data-sb-feedback="message:required">
-              Message is required.
+          <div className="d-flex align-items-center justify-content-center">
+            <div className="RetroButton">
+              Submit
             </div>
-            <div className="d-flex align-items-center justify-content-center">
-              <div className="RetroButton">
-                Submit
-              </div>
-            </div>
           </div>
-          <div className="col-lg-2">
-            {/* spacer */}
-          </div>
+        </div>
+        <div className="col-lg-2 col-md-2">
+          {/* spacer */}
         </div>
       </div>
     </div>

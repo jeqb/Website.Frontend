b8796a2b8901bde268b88b62398f3678ae82f30e
James <jameseqbonner@protonmail.com>
2021-10-26 20:52:25 -0700

    fleshing out services page more.

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './Services.css';
+import AzureLogo from './azure-logo.png';
+import DotnetLogo from './dotnet-logo.jpg';
+import ReactLogo from './react-logo.png';
+import SqlLogo from './sql-database-logo.png';
 
-const Services = () => {
-  return (
-    <div className="Content-Container">
-      <h1>
-        Services
-      </h1>
+/*
+
       <h3>
         Cloud Development
       </h3>
@@ -30,6 +29,70 @@ const Services = () => {
         Currently, I work as a fullstack developer where my frontend work is in Angular. My favorite framework
         however, is React (which is what this website is built on).
       </p>
+
+*/
+
+
+const Services = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-lg-2 col-md-2 col-sm-12">
+          <h1 className="text-center">
+            Services
+          </h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-2 col-md-2 col-sm-12 text-center">
+          <div>
+            <img
+              src={DotnetLogo}
+              alt=""
+              className="img-fluid"
+            />
+            <p className="text-center">
+              I am a .NET developer specalizing in .Net Core backend services.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-12 text-center">
+          <div>
+            <img
+              src={AzureLogo}
+              alt=""
+              className="img-fluid"
+            />
+            <p className="text-center">
+              Multiple years of web development experience in Microsoft Azure.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-12 text-center">
+          <div>
+            <img
+              src={ReactLogo}
+              alt=""
+              className="img-fluid"
+            />
+            <p className="text-center">
+              My frontend framework of choice is React.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-12 text-center">
+          <div>
+            <img
+              src={SqlLogo}
+              alt=""
+              className="img-fluid"
+            />
+          </div>
+          <p className="text-center">
+              Multiple years of web development experience in Microsoft Azure.
+            </p>
+        </div>
+      </div>
     </div>
   );
 };

a2d063faac9b030ff0a37d925035ec2b6d4f814f
James <jameseqbonner@protonmail.com>
2021-11-13 14:34:08 -0700

    removed comment

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -4,35 +4,6 @@ import DotnetLogo from './dotnet-logo.jpg';
 import ReactLogo from './react-logo.png';
 import SqlLogo from './sql-database-logo.png';
 
-/*
-
-      <h3>
-        Cloud Development
-      </h3>
-      <p className="Description">
-        My career is in Cloud development - specifically in Microsoft Azure. I have experience with
-        Azure DevOps CI/CD, Azure App Services (with and without containerization), and Azure functions.
-      </p>
-      <br/>
-      <h3>
-        Backend
-      </h3>
-      <p className="Description">
-        Backend web development is how I got started. I've done extensive backend web services in both
-        .Net Core as well as python based ML systems.
-      </p>
-      <br/>
-      <h3>
-        Frontend
-      </h3>
-      <p className="Description">
-        Currently, I work as a fullstack developer where my frontend work is in Angular. My favorite framework
-        however, is React (which is what this website is built on).
-      </p>
-
-*/
-
-
 const Services = () => {
   return (
     <div className="container-fluid">

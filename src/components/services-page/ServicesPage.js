3733936272c9251b0e20d1ed165a079f45902465
James <jameseqbonner@protonmail.com>
2021-12-02 20:55:20 -0700

    ServicesPage looks a little nicer

--- src/components/services-page/ServicesPage.js
+++ src/components/services-page/ServicesPage.js
@@ -6,7 +6,7 @@ import SqlLogo from './sql-database-logo.png';
 
 const ServicesPage = () => {
   return (
-    <div className="container-fluid d-flex flex-column" style={{ flexGrow: '1', background: 'green'}}>
+    <div className="container-fluid d-flex flex-column" style={{ flexGrow: '1' }}>
       <div className="row">
         <div className="col-lg-2 col-md-2 col-sm-12">
           <h1 className="text-center">
@@ -14,47 +14,47 @@ const ServicesPage = () => {
           </h1>
         </div>
       </div>
-      <div className="row flex-grow-1">
-        <div className="col-lg-2">
+      <div className="row flex-grow-1 align-items-center">
+        <div className="">
           {/* spacer */}
         </div>
-        <div className="col-lg-2 col-md-6 col-sm-12 text-center">
+        <div className="col-lg-6 col-md-6 col-sm-12 text-center">
           <div>
             <img
               src={DotnetLogo}
               alt=""
               style={{maxHeight: '20vh'}}
             />
-            <p className="text-center">
+            <p>
               I am a .NET developer specalizing in .Net Core backend services.
             </p>
           </div>
         </div>
-        <div className="col-lg-2 col-md-6 col-sm-12 text-center">
+        <div className="col-lg-6 col-md-6 col-sm-12 text-center">
           <div>
             <img
               src={AzureLogo}
               alt=""
               style={{maxHeight: '20vh'}}
             />
-            <p className="text-center">
+            <p>
               Multiple years of web development experience in Microsoft Azure.
             </p>
           </div>
         </div>
-        <div className="col-lg-2 col-md-6 col-sm-12 text-center">
+        <div className="col-lg-6 col-md-6 col-sm-12 text-center">
           <div>
             <img
               src={ReactLogo}
               alt=""
               style={{maxHeight: '20vh'}}
             />
-            <p className="text-center">
+            <p>
               My frontend framework of choice is React.
             </p>
           </div>
         </div>
-        <div className="col-lg-2 col-md-6 col-sm-12 text-center">
+        <div className="col-lg-6 col-md-6 col-sm-12 text-center">
           <div>
             <img
               src={SqlLogo}
@@ -62,11 +62,11 @@ const ServicesPage = () => {
               style={{maxHeight: '20vh'}}
             />
           </div>
-          <p className="text-center">
+            <p>
               Multiple years of web development experience in Microsoft Azure.
             </p>
         </div>
-        <div className="col-lg-2">
+        <div className="">
           {/* spacer */}
         </div>
       </div>
